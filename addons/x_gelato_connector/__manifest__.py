a85cec0785fa67c5bd4bfd9293df2b4265495060
ShinobiCat <itgod@croak.world>
2023-05-18 23:17:13 +0200

    x2317

--- addons/x_gelato_connector/__manifest__.py
+++ addons/x_gelato_connector/__manifest__.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
 {
     'name': "x_gelato_connector",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'version': '0.1',
+    'author': 'Croak',
+    'license': 'LGPL-3',
+    'website': 'https://github.com/ShinobiCat/odoo/tree/16.0/addons/x_gelato_connector',
+    'summary': 'Import products and place orders from Odoo to Gelato',
+    'description': 'Import products and place orders from Odoo to Gelato',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Invoicing',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
--- addons/x_plant_nursery/models/nursery.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ModelNurseryPlant(models.Model):
-    _name = 'model_nursery_plant'
-
-    name = fields.Char(string='Plant Name')
-    price = fields.Float()
